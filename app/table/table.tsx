18e109dfe6acfc3485000c02d0ab4fb5c2007316
Kev <sulpeum@gmail.com>
2023-05-14 20:33:38 -0400

    fix margin on pagination

--- app/table/table.tsx
+++ app/table/table.tsx
@@ -96,7 +96,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="mx-8 flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
