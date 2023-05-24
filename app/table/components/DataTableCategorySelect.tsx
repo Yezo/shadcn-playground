cff24abf562d2d6d3df3f90fa14d4726a37a3bae
Kev <sulpeum@gmail.com>
2023-05-24 17:59:20 -0400

    change dropdown icon to chevron

--- app/table/components/DataTableCategorySelect.tsx
+++ app/table/components/DataTableCategorySelect.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { useParams, useSearchParams } from "next/navigation"
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
-import { LayoutGridIcon, MapPinIcon } from "lucide-react"
+import { ChevronDownIcon, LayoutGridIcon, MapPinIcon } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -27,22 +27,22 @@ export function DataTableCategorySelect({ title, data }: Props) {
         <Button
           variant="outline"
           size="sm"
-          className="ml-auto hidden h-10 min-w-[200px] lg:flex"
+          className="ml-auto hidden h-10 min-w-[210px] lg:flex"
         >
           {title === "Region" ? (
-            <div className="flex items-center gap-2">
-              <MapPinIcon size={16} />
+            <div className="flex items-center justify-between gap-4">
               {region.replaceAll("%20", " ")}
+              <ChevronDownIcon size={16} />
             </div>
           ) : (
-            <div className="flex items-center gap-2">
-              <LayoutGridIcon size={16} />
+            <div className="flex items-center  justify-between gap-4">
               {category}
+              <ChevronDownIcon size={16} />
             </div>
           )}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="min-w-[200px]">
+      <DropdownMenuContent align="end" className="min-w-[210px]">
         {data.map((item, index) => {
           return (
             <DropdownMenuItem
