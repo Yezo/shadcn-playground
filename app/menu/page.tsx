70ce7682baa28feab991315f889bc51fbdbf220e
Kev <sulpeum@gmail.com>
2023-06-07 17:39:40 -0400

    remove import

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,14 +1,9 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+import { NavigationMenuDemo } from "@/app/menu/components/NavigationMenu"
 
 export default async function Page() {
-  return <div className="container mx-auto py-5 md:px-20">Menu</div>
+  return (
+    <div className="container mx-auto py-5 md:px-20">
+      <NavigationMenuDemo />
+    </div>
+  )
 }
