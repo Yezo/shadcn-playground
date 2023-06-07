fb4a4217c4ef021f8190dc346d9bfb78d7a5052e
Kev <sulpeum@gmail.com>
2023-06-07 17:37:05 -0400

    add imports

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,3 +1,14 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 export default async function Page() {
   return <div className="container mx-auto py-5 md:px-20">Menu</div>
 }
